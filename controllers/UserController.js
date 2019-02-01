706dfe198136db69d30caa926ab715eb410c8f1e
Denis Alayza <denis.alayza@gmail.com>
2019-02-01 03:50:31 -0200

    Optimiza remove localStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -209,21 +209,6 @@ class UserController {
     }
 
 
-    getUsersStorage() {
-
-        let users = [];
-
-        if (localStorage.getItem("users")) {
-
-            users = JSON.parse(localStorage.getItem("users"));
-
-        }
-
-        return users;
-
-    }
-
-
     selectAll() {
 
         let users = this.getUsersStorage();
